300d1728df14a21ece53d4c12c746dd2ca5d836b
heyefu 30002440 <heyefu@huawei.com>
2020-07-21 19:51:29 +0800

    登录过滤开启可配置

--- bus-web/src/main/java/com/tang/filter/LoginFilter.java
+++ bus-web/src/main/java/com/tang/filter/LoginFilter.java
@@ -6,6 +6,8 @@ import com.tang.entity.Config;
 import com.tang.error.ErrorCode;
 import com.tang.response.RestCode;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,6 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Objects;
 
 /**
  * 登录过滤器
@@ -23,25 +26,40 @@ import java.io.PrintWriter;
  **/
 @Slf4j
 public class LoginFilter implements HandlerInterceptor {
+    /**
+     * 是否需要开启登录过滤
+     */
+    private static final String REQUIRE = "bus.filter.login";
+
+    private static final String TOKEN = "user";
+
+    private static final String TRUE = "true";
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
-            Object handler) throws Exception {
-        if (checkUser(request.getSession().getAttribute("user"))) {
-            return true;
+        Object handler) throws Exception {
+        // filter中使用@Value不能获取值
+        Environment environment =
+            Objects.requireNonNull(WebApplicationContextUtils.getWebApplicationContext(request.getServletContext()))
+                .getBean(Environment.class);
+        if (TRUE.equals(environment.getProperty(REQUIRE))) {
+            if (checkUser(request.getSession().getAttribute(TOKEN))) {
+                return true;
+            }
+            returnJson(response);
         }
-        returnJson(response);
-        return false;
+        return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-            ModelAndView modelAndView) throws Exception {
+        ModelAndView modelAndView) throws Exception {
 
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
-            Exception ex) throws Exception {
+        Exception ex) throws Exception {
     }
 
     private boolean checkUser(Object user) {
